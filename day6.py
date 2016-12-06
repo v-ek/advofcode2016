cfb630e8a6cdbd937df75dbd6eca1f3dcfcdbebd
v-ek <vek@kth.se>
2016-12-06 07:26:57 +0100

    Day 6 done, Nice!

---
+++ day6.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+
+
+
+with open('input_d6.txt') as f: 
+    lines = [line.strip('\n') for line in f.readlines()]
+
+columns = zip(*lines)
+length = len(columns[0])
+
+message_list = []
+message_list_p2 = []
+for column in columns:
+    ctnr = Counter(column)
+    most_common = ctnr.most_common(1)[0][0]
+    least_common = ctnr.most_common()[-1][0]
+    message_list.append(most_common)
+    message_list_p2.append(least_common)
+
+message = ''.join(message_list)
+message_2 = ''.join(message_list_p2)
+
+print message
+print message_2
